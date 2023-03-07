1a544b6ced31b15533592c78bef54f8c16935688
Nikhil <nikhilkumar@iitbhilai.ac.in>
2023-03-07 18:29:30 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.uic import loadUi
+import sys
+
+class texteditor(QMainWindow):
+    def __init__(self):
+        super(Main, self).__init__()
+        loadUi("main.ui", self)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ui = texteditor()
+    ui.show()
+    app.exec_()
